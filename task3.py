aa6b5f8f8316fce9388b1d340457152040920e09
Praduamn <92991426+Praduamn@users.noreply.github.com>
2025-09-10 14:12:51 +0530

    website title scraper

--- task3.py
+++ task3.py
@@ -1,1 +1,19 @@
+import requests as r
+import re
 
+def scraper(url, output):
+  response = r.get(url)
+  match = re.search(r"<title>(.*?)</title>", response.text, re.IGNORECASE | re.DOTALL)
+  
+  if match:
+    title = match.group(1).strip()
+    with open(output,"w", encoding = "utf-8") as file:
+      file.write(title)
+    print(f"title saved: \"{title}\"")
+
+  else:
+    print("title not found :( ")
+
+if __name__ == "__main__":
+  url = input("enter url:")
+  scraper(url,"website_title.txt")
